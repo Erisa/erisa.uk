a7844402e34eb4e278a03b8fc8756d42a19b9262
Erisa <seriel@erisa.moe>
2019-11-22 19:04:48 +0000

    Add domains.html for cleaner display of domains

---
+++ script.js
@@ -0,0 +1,8 @@
+document.write('You\'re always free to reach out to me at my email address: ' + '<' + 'a' + ' ' + 'h' + 'r' + 'e' + 'f' + '=' + "'" + 'm' + 'a' + 'i' + 'l' + 't' + 'o' + '&' + '#' + '5' + '8' + ';' + '%' + '7' + '&' + '#' + '5' +
+'1' + ';' + 'e' + 'r' + '&' + '#' + '1' + '0' + '5' + ';' + 'e' + '%' + '6' + '&' + '#' + '6' + '7' + ';' + '&' + '#' + '6' + '4' + ';' + 'e' + '&' +
+'#' + '3' + '7' + ';' + '7' + '2' + 'i' + 's' + 'a' + '&' + '#' + '4' + '6' + ';' + 'm' + 'o' + '%' + '&' + '#' + '5' + '4' + ';' + '5' + "'" + '>' +
+'&' + '#' + '1' + '1' + '5' + ';' + 'e' + 'r' + 'i' + 'e' + 'l' + '&' + '#' + '6' + '4' + ';' + 'e' + 'r' + 'i' + 's' + 'a' + '&' + '#' + '4' + '6' +
+';' + '&' + '#' + '1' + '0' + '9' + ';' + 'o' + 'e' + '<' + '/' + 'a' + '>');
+
+// I'm inside a script anyway, might as well make the most of it.
+console.log("Hey there! You can view and contribute to the source of this website here: https://github.com/Erisa/erisa.moe\nWeb development is not something I usually do, so I appreciate any help.");
