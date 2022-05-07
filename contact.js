f12ac87ae96fa31743139e0667e740a0d0d5fde0
Erisa A <erisa@erisa.uk>
2022-05-07 14:52:11 +0000

    Use invisible hCaptcha

--- contact.js
+++ contact.js
@@ -1,39 +1,44 @@
-document.getElementById('submit').addEventListener('click', async (e) => {
-    e.preventDefault();
-  
-    console.log(document.getElementById('hcaptcha'))
-  
-    const name = document.getElementById('name').value;
-    const message = document.getElementById('message').value;
-    const captcha = hcaptcha.getResponse();
-  
-    if (name.trim() === '' || message.trim() === '' && captcha.trim() === '') {
-      document.getElementById('error').innerText = 'Please enter your name and message!'
-      return;
-    }
-  
-    const res = await fetch('https://erisa.uk/api/submit-contact', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name,
-        message,
-        captcha,
-      }),
-    });
-  
-    // It was sent successfully :)
-    if (res.status === 200) {
-      // Reset fields
-      document.getElementById('name').value = '';
-      document.getElementById('message').value = '';
-      hcaptcha.reset();
-  
-      document.getElementById('error').innerText = 'Sent!';
-    } else {
-      const response = await res.text();
-      document.getElementById('error').innerText = response;
-    }
-  })
+async function onSubmit(token) {
+  const name = document.getElementById('name').value;
+  const message = document.getElementById('message').value;
+  const captcha = token;
+
+  const res = await fetch('https://erisa.uk/api/submit-contact', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name,
+      message,
+      captcha,
+    }),
+  });
+
+  // It was sent successfully :)
+  if (res.status === 200) {
+    // Reset fields
+    document.getElementById('name').value = '';
+    document.getElementById('message').value = '';
+    //hcaptcha.reset();
+
+    document.getElementById('error').innerText = 'Sent!';
+  } else {
+    const response = await res.text();
+    document.getElementById('error').innerText = response;
+  }
+}
+
+function validate(event) {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const message = document.getElementById('message').value;
+
+  if (name.trim() === '' || message.trim() === '') {
+    document.getElementById('error').innerText = 'Please enter your name and message!'
+    return;
+  } else {
+    hcaptcha.execute();
+  }
+}
+
