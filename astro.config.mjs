1a5b84b97edb629c77a230c05322024916d62f7c
Erisa A <erisa@erisa.uk>
2023-01-21 05:32:14 +0000

    Remove trailing slashes in a weird way

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
-import sitemap from '@astrojs/sitemap';
-
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://erisa.uk',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx()],
+	build: {
+		format: 'file'
+	}
 });
