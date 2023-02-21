35bcf93e42b6b8df6901476f2df9711571acdebe
Erisa A <erisa@erisa.uk>
2023-02-21 22:02:49 +0000

    Run prettier

--- functions/api/clear-clearance/[[path]].js
+++ functions/api/clear-clearance/[[path]].js
@@ -1,22 +1,22 @@
 export function onRequestGet(ctx) {
-	let url = new URL(ctx.request.url)
+	let url = new URL(ctx.request.url);
 
-	url.pathname = '/' + ctx.params.path
-	url.protocol = 'https:'
+	url.pathname = '/' + ctx.params.path;
+	url.protocol = 'https:';
 
 	// for now lets just keep this simple
 	let domain = url.hostname;
 	if (url.hostname.endsWith('.erisa.uk')) {
-		domain = 'erisa.uk'
+		domain = 'erisa.uk';
 	} else if (url.hostname.endsWith('.erisa.pages.dev')) {
-		domain = 'erisa.pages.dev'
+		domain = 'erisa.pages.dev';
 	}
 
-	return new Response("", {
+	return new Response('', {
 		status: 302,
 		headers: {
-			'Location': url.toString(),
-			'Set-Cookie': `cf_clearance=invalid; expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0; path=/; domain=.${domain}`
-		}
-	})
+			Location: url.toString(),
+			'Set-Cookie': `cf_clearance=invalid; expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0; path=/; domain=.${domain}`,
+		},
+	});
 }
